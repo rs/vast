6ae3f7b1812ad34290782943da2fbdb98ba9d4e3
Yuto Suzuki <yuutoo.advance@gmail.com>
2019-11-20 14:03:01 +0900

    fix iframe resource using pointer for icon clicks

--- vast.go
+++ vast.go
@@ -426,7 +426,7 @@ type Icon struct {
 	// URL to a static file, such as an image or SWF file
 	StaticResource *StaticResource `xml:",omitempty"`
 	// URL source for an IFrame to display the companion element
-	IFrameResource CDATAString `xml:",omitempty"`
+	IFrameResource *CDATAString `xml:",omitempty"`
 	// HTML to display the companion element
 	HTMLResource *HTMLResource `xml:",omitempty"`
 }
