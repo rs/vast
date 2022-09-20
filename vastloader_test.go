6d176673b4f46882ebb6b25aa77a9d13ad0b99e6
Jack Wellborn <jwellborn@kargo.com>
2022-09-20 16:35:19 -0500

    Updated unit tests

--- vastloader_test.go
+++ vastloader_test.go
@@ -19,7 +19,7 @@ func TestLoadVAST(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "testdata/vast_inline_linear.xml")
 	}))
-	vastXml, err := vast.LoadVAST(server.URL+"/vast", 1000)
+	vastXml, err := vast.LoadURI(server.URL+"/vast", 1000)
 	assert.Nil(t, err)
 	assert.Equal(t, "601364", vastXml.Ads[0].ID)
 }
@@ -29,7 +29,7 @@ func TestLoadVASTSlowServer(t *testing.T) {
 		time.Sleep(time.Millisecond * 1000)
 		http.ServeFile(w, r, "testdata/vast_inline_linear.xml")
 	}))
-	vastXml, err := vast.LoadVAST(server.URL+"/vast", 1)
+	vastXml, err := vast.LoadURI(server.URL+"/vast", 1)
 	assert.Contains(t, err.Error(), "context deadline exceeded")
 	assert.Nil(t, vastXml)
 }
@@ -38,7 +38,7 @@ func TestLoadInvalidVAST(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "This is not VAST")
 	}))
-	vastXml, err := vast.LoadVAST(server.URL+"/vast", 5000)
+	vastXml, err := vast.LoadURI(server.URL+"/vast", 5000)
 	assert.Contains(t, err.Error(), "EOF")
 	assert.Nil(t, vastXml)
 }
@@ -47,7 +47,7 @@ func TestLoadInvalidURL(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(404)
 	}))
-	vastXml, err := vast.LoadVAST(server.URL+"/vast", 5000)
+	vastXml, err := vast.LoadURI(server.URL+"/vast", 5000)
 	assert.Contains(t, err.Error(), "EOF")
 	assert.Nil(t, vastXml)
 }
